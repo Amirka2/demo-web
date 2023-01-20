3904e978bc3ca61b49851ca946a13cf6ed72658b
Amir <70907990+Amirka2@users.noreply.github.com>
2023-01-20 15:37:03 +0500

    added date to service

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,14 +3,16 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.util.*;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts() {
         List<Post> posts = new ArrayList<Post>();
+        Date now = new Date(java.lang.System.currentTimeMillis());
         for (int i = 0; i < 3; i++) {
-            posts.add(new Post(String.format("%d", i)));
+            posts.add(new Post(String.format("%d", i), now));
         }
 
         return posts;
