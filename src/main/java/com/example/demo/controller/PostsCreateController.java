35eefaf0968997f48f4a20828c961916ccee3cfc
Amir <70907990+Amirka2@users.noreply.github.com>
2023-01-20 16:34:26 +0500

    added method doCreate to PostsCreateController

--- src/main/java/com/example/demo/controller/PostsCreateController.java
+++ src/main/java/com/example/demo/controller/PostsCreateController.java
@@ -1,17 +1,25 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostsCreateController {
+    @Autowired
+    PostService postsService;
+
     @RequestMapping(path = "/new", method = RequestMethod.GET)
     public String create(Model model) {
         model.addAttribute("appName", "Новый пост: ");
         return "create";
+    }
+
+    @RequestMapping(path = "/new", method = RequestMethod.POST)
+    public String doCreate(@ModelAttribute("text") String text) {
+        postsService.create(text);
+        return "redirect:/";
     }
 }
