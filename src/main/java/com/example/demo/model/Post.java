5a4fe4c7158449b3eb7eb0552663926aefed869b
Amir <70907990+Amirka2@users.noreply.github.com>
2023-02-01 14:41:52 +0500

    doesnt work correctly

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -7,22 +7,44 @@ public class Post {
     private String text;
     private Integer likes;
     private Date creationDate;
+    private long id;
 
-    public Post(String text, Date creationDate) {
+    public Post(long id, String text, Date creationDate) {
         this.text = text;
         this.creationDate = creationDate;
-        this.likes = (new Random().nextInt(1000));
+        this.likes = 0;
+        this.id = id;
     }
 
     public String getText() {
         return text;
     }
 
+    public void setText(String text) {
+        this.text = text;
+    }
+
     public Integer getLikes() {
         return likes;
     }
 
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
     public Date getCreationDate() {
         return creationDate;
+    }
+
+    public void setCreationDate(Date date) {
+        this.creationDate = date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
