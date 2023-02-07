d087e69a18f974ef1baebda0eb5c97a23099c2cd
Amir <70907990+Amirka2@users.noreply.github.com>
2023-02-07 22:01:55 +0500

    fixed. Changed imports from javax to jakarta in Post.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
+@EntityScan({"com.example.demo.model"})
 @SpringBootApplication
 public class DemoApplication {
 
